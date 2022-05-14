44bc9e2365fd0d091ba2d4bc27f5a9062506ccb7
MrPolymath <danielcarmona55@gmail.com>
2022-05-14 00:25:38 -0400

    Setup landing page

--- pages/[uuid].tsx
+++ pages/[uuid].tsx
@@ -1,10 +1,19 @@
-import type { NextPage } from "next";
+import type { NextPage, getServerSideProps, GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import Head from "next/head";
+
+const getServerSideProps: GetServerSideProps = async () => {
+  // Fetch data from external API
+  //   const res = await fetch(`https://localhost/api/save-image`)
+  //   const data = await res.json()
+  // Pass data to the page via props
+  //   return { props: { data } }
+};
 
 const Image: NextPage = () => {
   const router = useRouter();
   const { uuid } = router.query;
+
   return (
     <div>
       <Head>
@@ -16,14 +25,16 @@ const Image: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex justify-between items-center">
-        <div>
-          <h1>Mango Shot</h1>
-          <h2>{uuid}</h2>
+      <main className="flex items-center">
+        <div className="flex justify-center items-center">
+          <img
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c15d.png"
+            alt="mango"
+          />
         </div>
         <div className="flex items-center">
-          <h2>Tools:</h2>
-          <div className="flex items-center">add notes</div>
+          {/* <h2>Tools:</h2>
+          <div className="flex items-center">add notes</div> */}
         </div>
       </main>
       {/* <footer></footer> */}
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>Mango Shot</h1>
+      <main className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-6xl mb-4">Mango Sh🥭t</h1>
+        <h2 className="text-2xl mb-20">
+          Simple tool to take screenshots, annotate and share.
+        </h2>
+        <button
+          type="button"
+          class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        >
+          Install in Chrome
+        </button>
       </main>
       {/* <footer></footer> */}
     </div>
